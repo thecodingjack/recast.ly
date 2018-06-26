fd869d541877024785088170469c41ec959f4109
thecodingjack <jacklim.tkl@gmail.com>
2018-06-25 20:12:06 -0400

    completed mocha tests

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,10 +1,15 @@
-var VideoList = () => (
+var VideoList = (props) => (
+  // <div className="video-list">
+  //   <div><h5><em>videoListEntry</em> view goes here</h5></div>
+  //   <div><h5><em>videoListEntry</em> view goes here</h5></div>
+  //   <div><h5><em>videoListEntry</em> view goes here</h5></div>
+  //   <div><h5><em>videoListEntry</em> view goes here</h5></div>
+  //   <div><h5><em>videoListEntry</em> view goes here</h5></div>
+  // </div>
   <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
+    {props.videos.map((video)=>
+      <VideoListEntry video={video} handleClick={props.handleClick}/>
+    )}
   </div>
 );
 
--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,30 @@
 var searchYouTube = (options, callback) => {
-  // TODO
+  console.log(options)
+  $.ajax({
+    type: 'GET',
+    url: 'https://www.googleapis.com/youtube/v3/search',
+    cache: false,
+    
+    dataType: 'json',
+    timeout: 2000,
+    data: {
+      type: 'video',
+      key: options.key,
+      q: options.query,
+      maxResults: options.max,
+      part: 'snippet'
+    },
+    success: function(data){
+      console.log('success')
+      // callback(data)
+    },
+    error: function(error){
+      console.log('error')
+      // callback(error)
+    }
+  }).done(function(data){
+    callback(data.items)
+  })
 };
 
 window.searchYouTube = searchYouTube;
